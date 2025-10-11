8c57d718f677e3ca7aee239156b05142ead6722d
sokheangdo022-bit <sokheangdo022@gmail.com>
2025-10-11 22:59:07 +0700

    update V5

--- myscript.js
+++ myscript.js
@@ -4,92 +4,6 @@ var autoNumber = 1;
 var totalAmount = 0;
 var isDelivery = false;
 
-document.getElementById("btn-product").addEventListener("click", function () {
-
-    //GEt value
-    // var inputValue = document.getElementById("no").value;
-    var inputcustomer = document.getElementById("customer").value;
-    var inputphone = document.getElementById("phone").value;
-    var inputdesc = document.getElementById("desc").value;
-    var inputqty = document.getElementById("qty").value;
-    var inputprice = document.getElementById("price").value;
-    var inputaddress = document.getElementById("address").value;
-    // var inputamount = document.getElementById("amount").value;
-
-
-    // var iteam2 = {no: inputValue, name: inputcustomer};
-    //Set value
-    document.getElementById("cust-address").innerHTML = inputaddress;
-    document.getElementById("cust-phone").innerHTML = inputphone;
-    document.getElementById("cust-name").innerHTML = inputcustomer;
-
-
-    var inputamount = inputqty * inputprice;//20 45
-
-    totalAmount = totalAmount + inputamount; // Total Last Amount
-
-    document.getElementById("total").innerHTML = totalAmount;
-    document.getElementById("grand-total").innerHTML = totalAmount;
-
-    if (autoNumber > 10) {
-        return;
-    };
-
-    var iteam = {
-        no: autoNumber,
-        desc: inputdesc,
-        qty: inputqty,
-        price: inputprice,
-        disc: "",
-        amount: inputamount,
-    };
-
-    autoNumber = autoNumber + 1;
-
-
-    // autoNumber ++;
-    //{no: 1, name: "sokho", phone: 032165, decription: "colagen casing",qty : 2, unitprice: 25, amount: 50}
-
-    //Array - List
-    //["No001","sokho",12]
-    //[{no: inputValue, name: inputcustomer}]
-    //[{no: inputValue, name: inputcustomer},{no: inputValue, name: inputcustomer},12]
-
-    //[{no: 1, name: "sokho", phone: 032165, decription: "colagen casing",qty : 2, unitprice: 25, amount: 50},{no: 1, name: "sokho", phone: 032165, decription: "colagen casing",qty : 2, unitprice: 25, amount: 50}]
-
-    list.push(iteam);
-
-    // for(var j = 0; j < list.length; j++){
-    //     var a = list[j]["amount"];
-    //     totalAmount = totalAmount + a;
-    // }
-
-    renderTable();
-
-    //tomorrow Get value from Array add into Table
-    // togglePopup();
-
-});
-
-// function renderTable() {
-//     var tbody = document.querySelector("#invoiceTable tbody");
-//     tbody.innerHTML = ""; // clear previous rows
-
-//     list.forEach(function (item) {
-//         var row = `<tr>
-//             <td>${item.no}</td>
-//             <td>${item.desc}</td>
-//             <td>${item.qty}</td>
-//             <td>${item.price}</td>
-//             <td>${item.disc}</td>
-//             <td>${item.amount}</td>
-//         </tr>`;
-//         tbody.innerHTML += row;
-//     });
-
-
-// }
-
 function renderTable() {
     const tbody = document.querySelector("#invoiceTable tbody");
     tbody.innerHTML = ""; // clear previous rows
@@ -126,13 +40,13 @@ function renderTable() {
                     renderTable(); // re-render table
 
                     totalAmount = 0;
-                    for(var j = 0; j < list.length; j ++){
+                    for (var j = 0; j < list.length; j++) {
                         var item = list[j];
                         var tempAmount = item.amount;
-                        if(tempAmount != ""){
+                        if (tempAmount != "") {
                             totalAmount = totalAmount + tempAmount;
                         }
-                       
+
                     }
 
                     document.getElementById("total").innerHTML = totalAmount;
@@ -146,7 +60,6 @@ function renderTable() {
         tbody.appendChild(row);
     });
 }
-
 
 
 let today = new Date();
@@ -188,12 +101,6 @@ function autoInvoiceNumber(date) {
     return yyyy + "" + mmm + "" + dd + "" + currentSecond;
 }
 
-function togglePopup() {
-    const overlay = document.getElementById('popupOverlay');
-    overlay.classList.toggle('show');//Set class into element
-
-}
-
 
 function fakeObject(index) {
     var fakeitem = { //declare 
@@ -208,13 +115,37 @@ function fakeObject(index) {
     return fakeitem;
 }
 
-function closePopup() {
+generateTable();
 
-    togglePopup();
-    // onClickDeliveryCheckbox();
-    // var checkbox = document.getElementById("deliveryCheck");
-    // isDelivery = checkbox.checked;
+var checkbox = document.getElementById("deliveryCheck");
+checkbox.addEventListener("change", function () {
+    var boxDelivery = document.getElementById("box-deliveryid");
+    if (checkbox.checked) {
+        isDelivery = true;
+        boxDelivery.style.display = "block";
+    } else {
+        isDelivery = false;
+        boxDelivery.style.display = "none";
+    }
+    generateTable();
+});
 
+function printInvoice() {
+    var invoiceForm = document.getElementById("form-containerid");
+    var buttonPrint = document.getElementById("buttonprint");
+    invoiceForm.style.display = "none";
+    buttonPrint.style.display = "none";
+    window.print();
+}
+
+var titleInvoice = document.getElementsByClassName("title")[0];
+titleInvoice.addEventListener("mouseover", function () {
+    var buttonPrint = document.getElementById("buttonprint");
+    buttonPrint.style.display = "block";
+});
+
+
+function generateTable() {
     var fakeCount = 10 - list.length;  // 10 -2 = 8
 
     if (fakeCount > 0) {
@@ -246,39 +177,16 @@ function closePopup() {
             }
             list.push(fakeitem);
         }
+    } else {
+        if (isDelivery == true) {
+            var boxDelivery = document.getElementById("box-deliveryid").value;
+            list[9].disc = "Delivery";
+            list[9].amount = parseFloat(boxDelivery);
+        } else {
+            list[9].disc = "";
+            list[9].amount = "";
+        }
     }
+
     renderTable();
 }
-var checkbox = document.getElementById("deliveryCheck");
-
-checkbox.addEventListener("change", function () {
-    var boxDelivery = document.getElementById("box-deliveryid");
-    if (checkbox.checked) {
-        isDelivery = true;
-        boxDelivery.style.display = "block";
-    } else {
-        isDelivery = false;
-        boxDelivery.style.display = "none";
-    }
-
-});
-function printInvoice() {
-
-    var invoiceForm = document.getElementById("form-containerid");
-    var buttonPrint = document.getElementById("buttonprint");
-    invoiceForm.style.display = "none";
-    buttonPrint.style.display = "none";
-
-    window.print();
-
-}
-var titleInvoice = document.getElementsByClassName("title")[0];
-titleInvoice.addEventListener("mouseover", function () {
-    var buttonPrint = document.getElementById("buttonprint");
-
-    buttonPrint.style.display = "block";
-
-    console.log("mouse over")
-});
-
-
